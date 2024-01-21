97a2a54773a719adc7130b7e0cf834d97de023bb
Chedrian07 <seungjung0711@gmail.com>
2024-01-22 05:42:29 +0900

    Let's G0 Project

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from openai import OpenAI
 import os
 import time
+import sys
+
+
 
 client = OpenAI(
     api_key=os.getenv("OPENAI_API"),
@@ -12,6 +15,13 @@ def check_api():
         raise Exception("Please set your OPENAI_API environment variable.") 
     else:
         print("API is set")
+        
+        
+def seek_info():
+    file_path = sys.argv[1]
+    output = os.popen(f"python seek_info.py {file_path}").read()
+    print(output)
+
 
 def set_ai():
     model = "gpt-3.5-turbo"
@@ -25,9 +35,9 @@ def set_ai():
     )
     
     print(response)
-    
 
 
 if __name__ == "__main__":
     check_api()
+    seek_info()
     set_ai()
---
+++ test/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void test(){
+
+	for(int i=0;i<10;i++){
+	
+		printf("*");
+
+	}
+
+}
+
+
+int main(){
+
+	
+	printf("Hello WORLD");
+	test();
+
+
+}
